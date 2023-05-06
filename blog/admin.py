7e65e29608e2dee91fdc5e4d956e6444a0b67991
winstonmhango23 <winstonmhango23@gmail.com>
2023-05-06 10:29:32 +0200

    added index view

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from blog.models import Tag, Post,  Comment
 # Register your models here.
 
-admin.site.register(Tag)
+# admin.site.register(Tag)
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+# import blog.views
+# from django.urls import path
+# urlpatterns = [
+#     # other patterns
+#     path("", blog.views.index)
+# ]
